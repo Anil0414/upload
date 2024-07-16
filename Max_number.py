7b7fac4f07b8a552c421155a27dcf0bd1a1b80c9
Anil0414 <venkat1238c@gmail.com>
2024-07-16 00:14:38 +0000

    My Code chnages

---
+++ Max_number.py
@@ -0,0 +1,9 @@
+list = [1, 2, 3, 44, 5, 6, 7]
+
+# Sort the list
+list.sort()
+
+# Get the maximum value (last element in the sorted list)
+max_result = list[-1]
+
+print('Max number in the list:', max_result)
